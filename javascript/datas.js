6d3caf751b28660adbf1bdf50e6ccd50e365fd35
Camille Courtois <cc.camille.courtois@gmail.com>
2015-08-02 17:33:24 +0100

    Attempt to style article a little. Time to go to the pub

--- javascript/datas.js
+++ javascript/datas.js
@@ -1,10 +1,10 @@
 var article = {
 	title: 'Where to get a good baguette in Stratford?',
-	content: 'Go to a good bakery, duh.'
+	p: 'Go to a good bakery, duh.'
 };
 
 
 $(function(){
-	$('.article__title').text(article.title);
-	$('.article__content').text(article.content);
+	$('.article-text__title').text(article.title);
+	$('.article-text__p').text(article.p);
 });
