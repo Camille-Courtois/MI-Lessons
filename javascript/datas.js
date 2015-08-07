d5af49387a87b3b41c24dc3a469c1a1702c77487
Camille Courtois <cc.camille.courtois@gmail.com>
2015-08-07 06:39:40 +0100

    Attempt to organisew my datas -need to work on jQuery

--- javascript/datas.js
+++ javascript/datas.js
@@ -1,10 +1,17 @@
-var article = {
-	title: 'Where to get a good baguette in Stratford?',
-	p: 'Go to a good bakery, duh.'
+var article = [
+	{id: 'baguette', title: 'Where to get a good baguette in Stratford?', p: 'Go to a good bakery, duh.'},
+	{id: 'frogs', title: 'Where to buy a frog', p: 'Go back to France!'}
+];
+
+
+function callArticle (idHere) {
+//loop through all articles
+	for(var i = 0; i<article.length; i++){
+		if(idHere==article[i].id){
+	$('.article-text__title').text(article[i].title);
+	$('.article-text__p').text(article[i].p);
+		}
+	}
 };
 
-
-$(function(){
-	$('.article-text__title').text(article.title);
-	$('.article-text__p').text(article.p);
-});
+callArticle ('baguette');
