27e1be721219432cee682d3910d0317f3b6732e0
Camille Courtois <cc.camille.courtois@gmail.com>
2015-08-08 14:11:56 +0100

    Change text indent on click
    Signed-off-by: Camille Courtois <cc.camille.courtois@gmail.com>

--- javascript/datas.js
+++ javascript/datas.js
@@ -6,8 +6,8 @@ var article = [
 function test(idHere){	//$?
 	for(var i = 0; i<article.length; i++){
 		if(idHere==article[i].id){
-			$('.article-text__title').text(article[i].title);
-			$('.article-text__p').text(article[i].p);
+			$('.article-content__title').text(article[i].title);
+			$('.article-content__p').text(article[i].p);
 		}
 	}
 };
---
+++ javascript/text-indent.js
@@ -0,0 +1,4 @@
+function textIndent() {
+	$('.article-content__title').css('text-indent', '-9999px');
+	$('.article-content__p').css('text-indent', '0px');
+}
