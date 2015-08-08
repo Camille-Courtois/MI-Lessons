bcaf471c4122a75e72d905a114c3067a63eb0d99
Camille Courtois <cc.camille.courtois@gmail.com>
2015-08-08 11:35:37 +0100

    Attempt to create a function to inject datas in the doc + Style question article box

--- javascript/datas.js
+++ javascript/datas.js
@@ -1,17 +1,13 @@
 var article = [
-	{id: 'baguette', title: 'Where to get a good baguette in Stratford?', p: 'Go to a good bakery, duh.'},
-	{id: 'frogs', title: 'Where to buy a frog', p: 'Go back to France!'}
+	{id:'baguette',	title: 'Where to get a good baguette in Stratford?', p: 'Go to a good bakery, duh.'},
+	{id:'other', title: 'question', p: 'whatever'}
 ];
 
-
-function callArticle (idHere) {
-//loop through all articles
+function test(idHere){	//$?
 	for(var i = 0; i<article.length; i++){
 		if(idHere==article[i].id){
-	$('.article-text__title').text(article[i].title);
-	$('.article-text__p').text(article[i].p);
+			$('.article-text__title').text(article[i].title);
+			$('.article-text__p').text(article[i].p);
 		}
 	}
 };
-
-callArticle ('baguette');
