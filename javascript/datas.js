d15e7c63432d9e8c46efbb07ca15025c92c5c060
Camille Courtois <cc.camille.courtois@gmail.com>
2015-08-14 15:34:11 +0100

    Little tweaks

--- javascript/datas.js
+++ javascript/datas.js
@@ -3,7 +3,7 @@ var article = [
 	{id:'other', title: 'question', p: 'whatever'}
 ];
 
-function test(idHere){	//$?
+function test(idHere){
 	for(var i = 0; i<article.length; i++){
 		if(idHere==article[i].id){
 			$('.article-content__title').text(article[i].title);
